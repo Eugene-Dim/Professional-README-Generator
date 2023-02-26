2a814e0d9374887100ca2f1bc278a2ddc5ba86b4
Eugene Dimov <eugenedostoevsky@Eugenes-MBP.MG8702>
2023-02-25 19:16:11 -0800

    .

---
+++ develop/develop/index.js
@@ -0,0 +1,102 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./util/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+ // project title
+ {
+    name: 'title',
+    type: 'input',
+    message: 'What is the title of your project?',
+  },
+  // description
+  {
+    name: 'description',
+    type: 'input',
+    message: 'Provide a short description explaining the what, why, and how of your project.',
+  },
+// installation
+  {
+    name: 'installation',
+    type: 'input',
+    message: 'Describe the installation instructions for your project.',
+  },
+// usage info
+  {
+    name: 'usage',
+    type: 'input',
+    message: 'What is the usage of your project?',
+  },
+  // contribution
+  {
+    name: 'contribution',
+    type: 'input',
+    message: 'List any collaborators of your project.',
+  },
+// test instructions
+{
+    name: 'test',
+    type: 'input',
+    message: 'Include any test you have done for your project.',
+  },
+  // license badge
+  {
+    name: 'license',
+    type: 'list',
+    message: 'Pick an appropriate license for your project.',
+    choices: [
+    'Apache',
+    'GNU GPLv3',
+    'MIT',
+    'None'],
+  },
+  // GitHub username
+  {
+    name: 'github',
+    type: 'input',
+    message: 'Please enter your GitHub username.',
+  },
+  // email
+  {
+    name: 'email',
+    type: 'input',
+    message: 'Please enter your email.',
+  },
+
+];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        data.getLicense = getLicense(data.license)
+        fs.writeFile('generatedREADME/README.md', generateMarkdown(data), (err) => {
+            if (err) {
+                return console.log(err)
+            }
+            return console.log('README Generated!')
+        })
+    })
+}
+
+function getLicense(value){
+    switch(value) {
+        case 'Apache':
+        return '![License: Apache](https://img.shields.io/badge/license-Apache%202.0-yellow)';
+ 
+        case 'GNU GPLv3':
+        return '![License: GNU GPLv3](https://img.shields.io/badge/license-GNU%20GPL-green)';
+       
+        case 'MIT':
+        return '![License: MIT](https://img.shields.io/badge/license-MIT-blue)';
+ 
+        case 'None':
+        return '![License: None](https://img.shields.io/badge/license-none-orange)';        
+    }
+}
+
+init();
---
+++ develop/generatedREADME/README.md
@@ -0,0 +1,39 @@
+
+    # Project
+   
+    ![License: Apache](https://img.shields.io/badge/license-Apache%202.0-yellow)
+   
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributions](#contributions)
+    * [License](#license)
+    * [Tests](#tests)
+    * [Questions](#questions)
+   
+    ## Description
+    Testing if the project works
+   
+    ## Installation
+    need to install node
+   
+    ## Usage
+    testing if it works
+   
+    ## License
+    ![License: Apache](https://img.shields.io/badge/license-Apache%202.0-yellow)
+   
+    ## Contributions
+    Me
+   
+    ## Tests
+    Testing right now
+   
+    ## Questions
+    If you have any questions or concerns you can visit my GitHub portfolio using the link provided or contact me via email provided below.
+
+    GitHub: https://github.com/eugene-dim
+
+    Email: dimovjenea@gmail.com
+  
